cabf32d81bb9bc29a06eecfb2a461a9bc642e8de
mapoga <mathieugouletaubin@gmail.com>
2018-03-28 18:15:02 -0400

    No Menus

--- Tools/Math/Matrix 3x3/init.py
+++
@@ -1,9 +0,0 @@
-import nuke
-import os
-
-# Add sub directories to plugin path
-initDir = os.path.dirname(os.path.realpath(__file__))
-for i in os.listdir(initDir):
-	subDir = os.path.join(initDir, i)
-	if os.path.isdir(subDir):
-		nuke.pluginAddPath( subDir )
